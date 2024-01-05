7000f1944460ff36448ff6385df9995d438a2b91
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2024-01-05 17:44:39 -0500

    Expanded upon method documentation
    Provided further information regarding usage of TableBlock

--- UIElements/TableBlock.cs
+++ UIElements/TableBlock.cs
@@ -11,14 +11,24 @@ namespace ConsoleUI.UIElements
         private readonly ConsoleColor _headerForegroundColor;
         private readonly ConsoleColor _headerBackgroundColor;
         private readonly bool _displayHeaders;
-
         /// <summary>
-        /// Constructor for TableBlock.
+        /// Initializes a new instance of the TableBlock class with specified headers and optional color settings.
         /// </summary>
         /// <param name="headers">Column headers for the table.</param>
-        /// <param name="headerForegroundColor">Foreground color for the headers.</param>
-        /// <param name="headerBackgroundColor">Background color for the headers.</param>
-        /// <param name="displayHeaders">Flag to indicate whether to display headers.</param>
+        /// <param name="headerForegroundColor">Foreground color for the headers. Default is <see cref="ConsoleColor.White"/>.</param>
+        /// <param name="headerBackgroundColor">Background color for the headers. Default is <see cref="ConsoleColor.Black"/>.</param>
+        /// <param name="displayHeaders">Flag to indicate whether to display headers. Default is true.</param>
+        /// <exception cref="ArgumentNullException">Thrown when headers is null.</exception>
+        /// <example>
+        /// This example shows how to create a TableBlock with headers and custom colors:
+        /// <code>
+        /// var table = new TableBlock(
+        ///     new[] { "Header1", "Header2" },
+        ///     ConsoleColor.Yellow,
+        ///     ConsoleColor.DarkBlue
+        /// );
+        /// </code>
+        /// </example>
         public TableBlock(string[] headers,
                           ConsoleColor headerForegroundColor = ConsoleColor.White,
                           ConsoleColor headerBackgroundColor = ConsoleColor.Black,
@@ -34,9 +44,23 @@ namespace ConsoleUI.UIElements
         /// Add a row to the table.
         /// </summary>
         /// <param name="row">The row to add. Must match the number of headers.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the row is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when the row does not have the same number of elements as the headers.</exception>
+        /// <example>
+        /// This example shows how to add a row to a table with two headers:
+        /// <code>
+        /// var table = new TableBlock(new[] { "Header1", "Header2" });
+        /// table.AddRow(new[] { "Row1 Col1", "Row1 Col2" });
+        /// </code>
+        /// </example>
         public void AddRow(string[] row)
         {
-            if (row == null || row.Length != _headers.Length)
+            if (row == null)
+            {
+                throw new ArgumentNullException(nameof(row), "Row cannot be null.");
+            }
+
+            if (row.Length != _headers.Length)
             {
                 throw new ArgumentException("Row must have the same number of elements as there are headers.", nameof(row));
             }
